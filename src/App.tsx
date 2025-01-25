1edbe7e09b28e467218dd1b054c3718a850d359a
Rinel <rineliniguezsosa@gmail.com>
2025-01-24 19:31:54 -0600

    [FEAT] Estableci un simple h1 con un mensaje para observar la total funcionalidad de tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! This is my new portfolio
+      </h1>
     </>
   )
 }
