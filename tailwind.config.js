bbdbe1502bd81dc6d6091abdbafb119e4bd97eb8
Rinel <rineliniguezsosa@gmail.com>
2025-01-24 19:57:04 -0600

    [FEAT] Configurando los colores de nuestro portafolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors:{
+            dark:{
+                DEFAULT: '#080808',
+                600:'#1B1B1B'
+            },
+            darkgray:'#9C9C9C',
+            blue:'#0d9aff',
+            white:'#FFFFFF'
+        }
+      },
     },
     plugins: [],
   };
