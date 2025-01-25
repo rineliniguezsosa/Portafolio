788e681135fe33c80964cc39c745cc6ab44281b6
Rinel <rineliniguezsosa@gmail.com>
2025-01-24 20:47:58 -0600

    [FEAT] Multiples cambios en el App.tsx
    - estableciendo el elemento html main con los atributos css width-full, hight-screen, tamaño y color de borde
    - estableciendo el elemento html section con los atributos css, width-full, tamaño y color de borde
    - estableciendo el elemento html header con los atributos css, width-full padding de 10 y tamaño y color de borde

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world! This is my new portfolio
-      </h1>
-    </>
+    <main className='w-full h-screen bg-green border-2 border-red-500'>
+        <section className='w-full border-2 border-red-500'>
+          <header className='w-full p-10 border-2 border-blue-500'>
+          
+          </header>
+        </section>
+    </main>
   )
 }
 
