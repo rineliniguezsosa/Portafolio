a4a960e236c6a9c5533c0581fe03bf2465cb066a
Rinel <rineliniguezsosa@gmail.com>
2025-01-24 21:10:27 -0600

    [FEAT] Cambios en el App.tsx
    - estableciendo formato en el elemento header
    - estableciendo elementos div para mi imagen y descripción personal
    - atributos css en el div, width-full, padding en x de 5rem y tamaño y color de borde

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ function App() {
   return (
     <main className='w-full h-screen bg-green border-2 border-red-500'>
         <section className='w-full border-2 border-red-500'>
-          <header className='w-full p-10 border-2 border-blue-500'>
+          <header 
+            className='w-full p-10 border-2 border-blue-500 bg-dark-600'>
           
           </header>
+          <div className='w-full px-5 border-2 border-yellow-500'>
+            <div>
+
+            </div>
+            <div>
+              
+            </div>
+          </div>
         </section>
     </main>
   )
