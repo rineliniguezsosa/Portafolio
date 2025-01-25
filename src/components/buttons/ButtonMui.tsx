290d17541a1de6716412496a7dfb82ce2a321e1e
Rinel <rineliniguezsosa@gmail.com>
2025-01-25 16:26:26 -0600

    [FEAT] Cambios en el componente ButtonMui, estableciendo nuevas props
    - estableciendo las props component y location
    - la prop componente sirve para cambiar ó establecer el comportamiendo del botón
    - la prop href servira para el ancla

--- src/components/buttons/ButtonMui.tsx
+++ src/components/buttons/ButtonMui.tsx
@@ -2,9 +2,11 @@ import { Button } from "@mui/material";
 import { ButtonMuiProps } from "../../types/interfaces";
 
 
-export const ButtonMui = ({color,variant,size,width,title}:ButtonMuiProps) => {
+export const ButtonMui = ({component,location,color,variant,size,width,title}:ButtonMuiProps) => {
   return (
     <Button
+      component={component}
+      href={location}
       color={color}
       variant={variant}
       size={size}
