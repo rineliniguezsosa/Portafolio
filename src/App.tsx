1958c749aeccd6aab648b1b7ec647bb0bbfac201
Rinel <rineliniguezsosa@gmail.com>
2025-01-26 02:26:36 -0600

    [FEAT] Multiples cambios en el archivo App.tsx
    - eliminando importaciones de App.css y del componente Link de react router
    - eliminando el color de fondo en el elemento html main
    - eliminando el padding en x de 5 rem, flex box para alinear los elementos en row, gap de 5 rem para espaciado entre elementos, color de fondo, justificando los elementos al centro, padding en y de 20rem
    - estableci formato a los elementos
    - estableci comentarios delimitadores
    - cambios en la información personal, tamaño de caja al 60%, texto a la izquierda, tamaño y color de borde
    - css en el elemento h1, tamaño de texto a 1.5rem, fuente raleway, fuente bold, texto en color blanco

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-// import { Link } from 'react-router'
-import './App.css'
 import { ButtonMui } from './components/buttons/ButtonMui'
 import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
 import { Navbar } from './components/layout/Navbar';
 
 function App() {
   return (
-    <main className='w-full bg-green border-2 border-green-500'>
+    <main className='w-full border-2 border-green-500'>
         <section className='w-full border-2 border-red-500'>
           {/* <header 
             className='w-full p-10 border-2 border-blue-500 bg-dark-600'>
@@ -15,13 +13,24 @@ function App() {
               </nav>
           </header> */}
           <Navbar/>
-          <div className='w-full px-5 py-15 border-2 border-yellow-500'>
-            <div>
-                <img className='rounded-lg' src="../src/assets/images/rinelfoto.jpg" alt="rinel" />
+          <div 
+          className='w-full flex flex-col gap-5 bg-dark items-center py-20 border-2 border-yellow-500'
+          >
+
+            {/* imagen */}
+            <div className='w-3/5'>
+                <img 
+                  className='rounded-xl' 
+                  src="../src/assets/images/rinelfoto.jpg" 
+                  alt="rinel" 
+                />
             </div>
-            <div>
-              <h1>Rinel iñiguez sosa</h1>
-              <p>mi description</p>
+            {/* fin imagen */}
+
+            {/* información personal */}
+            <div className='w-3/5 text-left border-2 border-red-500'>
+              <h1 className='text-2xl font-raleway font-bold text-white'>Rinel iñiguez sosa</h1>
+              <p className='text-lg'>mi description</p>
               <ButtonMui
                 href="#"
                 endIcon={<KeyboardArrowRightOutlinedIcon />}
@@ -34,6 +43,7 @@ function App() {
                 <span className='text-xs'>¡Empecemos!</span>
               </ButtonMui>
             </div>
+            {/* fin información personal */}
             <div>
               
             </div>
