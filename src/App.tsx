fc0a4c641f20ed30de8b1a31e4c8e100be412bf9
Rinel <rineliniguezsosa@gmail.com>
2025-01-26 03:26:31 -0600

    [FEAT] Cambios en el archivo App.tsx
    - eliminando bordes
    - estableciendo css en los elementos p, color de texto darkgray, funete font-ibm-plex-mono, texto a 0.875rem
    - estableciendo un color de fondo y borde
    - estableciendo un width full y tamaño y color de borde

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { Navbar } from './components/layout/Navbar';
 
 function App() {
   return (
-    <main className='w-full border-2 border-green-500'>
-        <section className='w-full border-2 border-red-500'>
+    <main className='w-full border-2'>
+        <section className='w-full border-2 border-blue-500'>
           <Navbar/>
           <div 
-          className='w-full flex flex-col gap-5 bg-dark items-center py-20 border-2 border-yellow-500'
+          className='w-full flex flex-col gap-5 bg-dark items-center py-20 '
           >
 
             {/* imagen */}
@@ -22,23 +22,31 @@ function App() {
             {/* fin imagen */}
 
             {/* información personal */}
-            <div className='w-3/5 text-left border-2 border-red-500'>
+            <div className='w-3/4 text-left'>
               <h1 className='text-2xl font-raleway font-bold text-white'>Rinel iñiguez sosa</h1>
-              <p className='text-lg'>mi description</p>
+              <p className='text-darkgray font-ibm-plex-mono text-sm'>
+              I am a Full Stack Developer from Mérida, Yucatán, Mexico, specialized in JavaScript
+               technologies. My work has allowed me to grow professionally and explore places like Mexico City and Culiacán. 
+               Committed to delivering quality results, I value responsibility, dedication, and continuous improvement.
+              </p>
+              <br />
+              <p className='text-darkgray font-ibm-plex-mono text-sm'>
+              "Always looking to improve and deliver continuous results, contact me or discover my projects"
+              </p>
               <ButtonMui
                 href="#"
                 endIcon={<KeyboardArrowRightOutlinedIcon />}
                 className='text-white'
-                color='primary'
+                // color='primary'
                 variant='contained'
                 size='large'
-                sx={{ textTransform: "none" }}
+                sx={{ textTransform:"none",background:'#3F8E00',border:'1px #62BA1B solid'}}
               >
                 <span className='text-xs'>¡Empecemos!</span>
               </ButtonMui>
             </div>
             {/* fin información personal */}
-            <div>
+            <div className='w-full border-2 border-orange-500'>
               
             </div>
           </div>
