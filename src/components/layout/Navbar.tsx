5dfee9207e064cb49dcd37e886409a591a076192
Rinel <rineliniguezsosa@gmail.com>
2025-01-25 22:05:25 -0600

    [FEAT] Multiples cambios en el componente Navbar
    - pude hacer este componente rapidamente ya que lo he hecho en otro proyecto
    - importando IconButton,Drawer
    - importando el icono  MenuIcon
    - importando el hook useState
    - creando el state isDrawerOpen inicializada en false
    - pegue el contenido que tenia en App.tsx y complemente más cosas
    - en el componente IconButton estableci el método onclick para cerrar el drawer
    - estableci un div con una R tal vez la haga icono para mi inicial de mi nombre
    - estableci el componente Drawer en donde estableci la prop anchor para que se abra a la izquierda, pase la referencia de open para abrir y cerrar el drawer, estableci la prop onClose donde incialice el state en false y use PaperProps para establecer el width del drawer
    - estableci el elemento html nav en el contenido del componente Drawer
    - hasta aqui lo dejaré estoy algo cansado

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,38 @@
+import { IconButton,Drawer } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 
 
 export const Navbar = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   return (
-    <div>Navbar</div>
+    <header 
+    className='w-full py-10 px-5 border-2 border-blue-500 bg-dark-600 flex flex-row justify-between items-center'>
+
+      <IconButton onClick={()=> setIsDrawerOpen(!isDrawerOpen)}>
+              <MenuIcon></MenuIcon>
+      </IconButton>
+
+
+      <div>
+        <div>R</div>
+      </div>
+
+
+       <Drawer
+            anchor='left'
+            open={isDrawerOpen}
+            onClose={()=> setIsDrawerOpen(false)}
+            PaperProps={{
+              sx:{
+                width:'70%'
+              }
+            }}
+          >
+            <nav>
+              <span>Hola</span>
+            </nav>
+          </Drawer>
+  </header>
   )
 }
