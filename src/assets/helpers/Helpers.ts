986c1ce97c285ea9a773e90a9cf30037ad67b00c
Rinel <rineliniguezsosa@gmail.com>
2025-01-27 17:09:29 -0600

    [FEAT] Creando el archivo Helpers.ts en este archivo define el array de objetos que usare en mi componente TimelineMui
    - importando la interface workhistoryprops
    - creando el array de objetos Workhistory el cual tipe por workhistoryprops el cual será un array de objetos
    - estableci la estructura de mi array de objetos con la información de mi experiencia laboral

---
+++ src/assets/helpers/Helpers.ts
@@ -0,0 +1,13 @@
+import { workhistoryprops } from './../../types/interfaces';
+
+export const Workhistory: workhistoryprops[] = [
+    {
+        company:'Design and Conservation of DCIESA Properties - web developer',
+        role:'web developer',
+        duration:{
+            start:'August 2020',
+            end:'December 2020'
+        },
+        type:'internship'
+    },
+]
