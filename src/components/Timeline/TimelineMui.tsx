4a8c1ee583d71340ea9d613ab65f4ab0adec695f
Rinel <rineliniguezsosa@gmail.com>
2025-01-27 17:34:43 -0600

    [FEAT] Guardando los cambios en el componente TimelineMui
    - multiples importaciones TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, Timeline
    - importando la interfacace TimelineMuiProps
    - desestructurando las props de la interface TimelineMuiProps
    - estableciendo la estructura para el componente en la documentación oficial de material ui y estableciendp una data temporal
    - por el momento solo lo estoy guardando pero se mapeara la información

--- src/components/Timeline/TimelineMui.tsx
+++ src/components/Timeline/TimelineMui.tsx
@@ -1,7 +1,27 @@
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import { TimelineMuiProps } from '../../types/interfaces';
 
-
-export const TimelineMui = () => {
+export const TimelineMui = ({position,variant,color,experience}:TimelineMuiProps) => {
   return (
-    <div>Timeline</div>
+    <Timeline position={position}>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant={variant} color={color} />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+        {experience.map( data =>{
+          <h3>Design and Conservation of DCIESA Properties - web developer</h3>
+          <p>August 2020 - December 2020</p>
+        })
+        }
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
   )
 }
