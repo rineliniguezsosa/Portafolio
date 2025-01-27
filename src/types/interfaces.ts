4d3506fe46cc6944c66f0a4c16ee1a04db0fccf7
Rinel <rineliniguezsosa@gmail.com>
2025-01-27 17:05:55 -0600

    [FEAT] Cambios en el archivo interfaces
    - creando la interface workhistoryprops para tipar el contenido de mi experiencia laboral
    - tipando workhistoryprops, company, role duration, start, end y type como string
    - creando la interface TimelineMuiProps para tipar el componente TimelineMui
    - tipando las props de la interface TimelineMuiProps por, position : 'alternate-reverse'|'alternate'| 'left' | 'right' , variant como 'filled'|'outlined', content con el valor de workhistoryprops en cual espera un array de objetos

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -5,3 +5,18 @@ export interface ButtonMuiProps extends ButtonProps {
    href:string,
    className?:string
 }
+
+export interface workhistoryprops {
+   company:string,
+   role:string,
+   duration:{
+      start:string,
+      end:string
+   }
+   type:string
+}
+export interface TimelineMuiProps {
+   position:'alternate-reverse'|'alternate'| 'left' | 'right',
+   variant:'filled'|'outlined',
+   content: workhistoryprops[]
+}
