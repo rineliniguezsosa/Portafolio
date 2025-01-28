888fb2b8b91beb244d2837c66aca39cd1ead56f6
Rinel <rineliniguezsosa@gmail.com>
2025-01-28 14:59:47 -0600

    [FEAT] Multiples cambios en el componente TestimonialCard
    - estableciendo formato en el componente Card
    - eliminando la prop className
    - estableciendo la prop sx para el css en donde estableci los atributos, backgroundColor:'#080808',display:'flex',flexDirection:'column'
    - css en el elemento blockquote, color de texto text-darkgray, tamaño de texto a 0.875rem,  fuente ibm-plex-mono
    - estableciendo un width al 50%
    - estableciendo el elemento html img con un width y height de 10rem y aplicando un bordes radius por completo, margin-right de 3rem
    - estableciendo el elemento span con los atributos, tamaño de texto a 1.125rem, fuente raleway, funete bold y a color blanco

--- src/components/Card/TestimonialCard.tsx
+++ src/components/Card/TestimonialCard.tsx
@@ -2,16 +2,23 @@ import { Card } from "@mui/joy"
 import { TestimonialCardProps } from "../../types/interfaces"
 
 
-export const TestimonialCard = ({id,name,description,position}:TestimonialCardProps) => {
+export const TestimonialCard = ({id,name,description,img,position}:TestimonialCardProps) => {
   return (
-    <Card key={id} className="flex flex-col bg-dark" orientation="horizontal" size="lg" variant="outlined">
+    <Card 
+       key={id} 
+       sx={{backgroundColor:'#080808',display:'flex',flexDirection:'column'}} 
+       orientation="horizontal" 
+       size="lg" 
+       variant="outlined"
+    >
       <div className="w-full border-2 border-red-500">
-        <blockquote>
+        <blockquote className="text-darkgray text-sm font-ibm-plex-mono">
             {description}
         </blockquote>
       </div>
-      <div>
-         {name}{position}
+      <div className="w-1/2">
+         <img className="w-10 h-10 rounded-full mr-3" src={img} alt={name} />
+         <span className="text-lg font-raleway font-bold text-white">{name}{position}</span>
       </div>
     </Card>
   )
