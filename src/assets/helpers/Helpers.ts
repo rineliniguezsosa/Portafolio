0bf7e9b32ca4fd4ee875c87026585877570c6a4e
Rinel <rineliniguezsosa@gmail.com>
2025-01-27 18:39:23 -0600

    [FEAT] Estableciendo mi experiencia profesional en el array de objetos Workhistory

--- src/assets/helpers/Helpers.ts
+++ src/assets/helpers/Helpers.ts
@@ -9,5 +9,31 @@ export const Workhistory: workhistoryprops[] = [
             end:'December 2020'
         },
         type:'internship'
+    },{
+        company:'Paynalli systems',
+        role:'Software developer',
+        duration:{
+            start:'March 2022',
+            end:'January 2023'
+        },
+        type:'hybrid'
     },
+    {
+        company:'Sol4it',
+        role:'Front end angular Developer',
+        duration:{
+            start:'',
+            end:''
+        },
+        type:'remote'
+    },
+    {
+        company:'Caprepa',
+        role:'Full stack developer',
+        duration:{
+            start:'September 2023',
+            end:'June 2024'
+        },
+        type:'on-site'
+    }
 ]
