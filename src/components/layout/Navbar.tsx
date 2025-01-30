c8a710d5d4259b55e592e2868da6d89aed96efb9
Rinel <rineliniguezsosa@gmail.com>
2025-01-29 18:52:18 -0600

    [FEAT] Guardando los cambios en el Navbar
    - comentando la importación de Link de react router
    - eliminare esta dependencia
    - modificando el padding del elemento nav, padding en x a 2rem y padding en y de 5rem
    - eliminando bordes
    - eliminando la coma de el elemento span
    - estableciendo un comentario para los links principales
    - css en el div width full, display flex para que los elementos estén en dirección a column , margin-top de 5rem
    - cambios en los elementos a , use el atributo href para dirigirme a la sección de #work-experience como son anclas bueno, estableci el método onclick para cerrar el drawer, estableciendo css al elemento a, estableciendo formato al elemento a
    - creo indudablemente que esto lo voy a pasar a un componente ya que se repetira pero por el momento guardamos los cambios temporalmente para que se observen cambios

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,6 +2,7 @@ import { IconButton,Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react';
+// import { Link } from 'react-router';
 
 
 export const Navbar = () => {
@@ -42,13 +43,27 @@ export const Navbar = () => {
             }}
             // className="bg-dark"
           >
-            <nav className='w-full h-full px-10 py-2 flex flex-col items-center'>
-              <div className='w-full flex justify-between border-2 border-red-500'>
-                <span className='text-white font-raleway font-bold font-xl'>Hola Bienvenido,</span>
+            <nav className='w-full h-full px-2 py-5 flex flex-col items-center'>
+              <div className='w-full flex justify-between'>
+                <span className='text-white font-raleway font-bold font-xl'>Hola Bienvenido</span>
                 <CloseIcon sx={{color:'white'}} onClick={()=> setIsDrawerOpen(false)}/>
               </div>
 
-              <div>
+              {/* links principales */}
+              <div className='w-full flex flex-col mt-5'>
+              <a 
+                href='#work-experience' 
+                onClick={() => setIsDrawerOpen(false)} 
+                className='w-full text-white text-center px-5 py-5'>
+                Work experience
+              </a>
+              <a 
+                href='#testimonial' 
+                onClick={() => setIsDrawerOpen(false)} 
+                className='w-full text-white text-center px-5 py-5'
+              >
+                Testimonial
+              </a>
 
               </div>
             </nav>
