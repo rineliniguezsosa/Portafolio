0eb528f1ece2d78ef86bfa0707936ac1983d2b07
Rinel <rineliniguezsosa@gmail.com>
2025-02-02 18:02:16 -0600

    [FEAT] Guardando temporalmente el componente  ProjectCard en donde le pasare como prop un titulo y recibira todas las props de la img, descripción, url hacia el proyecto, url si el proyecto esta en live mode etc
    - guardo los cambios temporalmente ya que estoy maquetando como se veria el card

--- src/components/Card/ProjectCard.tsx
+++ src/components/Card/ProjectCard.tsx
@@ -1,7 +1,39 @@
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
 
 export const ProjectCard = () => {
   return (
-    <div>ProjectCard</div>
+    <>
+      <h4>Titulo</h4>
+      <Card sx={{border:'none'}}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+            />
+          <CardContent>
+            <Typography gutterBottom variant="h3" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+        </CardActions>
+      </Card>
+    </>
   )
 }
