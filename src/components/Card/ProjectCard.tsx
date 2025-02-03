8df641541cd8533d27a42ff7121a22f4cd9d34d6
Rinel <rineliniguezsosa@gmail.com>
2025-02-02 19:46:23 -0600

    [FEAT] Cambiando la estructura del componente ProjectCard
    - Eliminando importaciones de material ui por que las elimine ya que para el carrousel se usan los elementos de material ui/joy
    - moviendo el contenido de la prop sx para establecer css y pegando el contenido css anterior de testimonialcard
    - estableciendo el condicional rendering para validar la prop img ya que no sera obligatoria
    - agregando el componente AspectRadio con la prop ObjecFit en cover para que la imagen ocupe todo el contenedor
    - eliminando el contenido del card
    - estableci elementos como h4, p y a

--- src/components/Card/ProjectCard.tsx
+++ src/components/Card/ProjectCard.tsx
@@ -1,42 +1,37 @@
-import { Card } from "@mui/joy"
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardActions from '@mui/material/CardActions';
+import { AspectRatio, Card } from "@mui/joy"
 import { projectProps } from "../../types/interfaces";
 
 export const ProjectCard = ({id,name,description,img,url}: projectProps) => {
   return (
       <Card
+        sx={{  
+          border:'none',
+          display:'flex',
+          flexDirection:'column',
+          justifyContent:'space-between',
+          paddingX:2,
+        }} 
         key={id}
-        sx={{border:'none'}}
         orientation="horizontal" 
         size="sm" 
         variant="outlined"
+
        >
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image={img}
-            alt="green iguana"
-            />
-          <CardContent>
-            <Typography gutterBottom variant="h3" component="div">
-              {name}
-            </Typography>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              {description}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button href={url} size="small" color="primary">
-            Share
-          </Button>
-        </CardActions>
+        {
+          img && (
+          <AspectRatio objectFit="cover">
+            <img src={img} alt="" />
+          </AspectRatio>
+          )
+        }
+  
+        <div>
+            <h4>{name}</h4>
+            <p>{description}</p>
+
+            <a href={url}>Know more</a>
+        </div>
+        
       </Card>
   )
 }
