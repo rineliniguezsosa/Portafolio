729c87a2f98abe14efc6667989b3c29e4ea7da5f
Rinel <rineliniguezsosa@gmail.com>
2025-02-02 19:06:10 -0600

    [FEAT] Cambios en el CarrouselMui
    - desestructirando la prop title
    - esdtableciendo un grafment como envoltotio de nuestro componente
    - estableciendo el elemento div con el elemento h4 donde ira el titulo de los proyectos, react native, react, javascript, front end mentor etc
    - atributos css en el elemento h4 text-darkgray,texto a 0.875rem, fuente ibm-plex-mono

--- src/components/Carrousel/CarrouselMui.tsx
+++ src/components/Carrousel/CarrouselMui.tsx
@@ -2,25 +2,30 @@ import Box from '@mui/joy/Box';
 import { CarrouselMuiProps } from '../../types/interfaces';
 
 
-export const CarrouselMui = ({children}:CarrouselMuiProps) => {
+export const CarrouselMui = ({title,children}:CarrouselMuiProps) => {
   return (
-    <Box
-    sx={{
-      display: 'flex',
-      gap: 1,
-      py: 1,
-      overflow: 'auto',
-      width: '100%',
-      scrollSnapType: 'x mandatory',
-      '& > *': {
-        flex: '0 0 auto',
-        width: 'calc(100% - 16px)',
-        scrollSnapAlign: 'center',
-      },
-      '::-webkit-scrollbar': { display: 'none' },
-    }}
-  >
-    {children}
-  </Box>
+    <>
+      <div>
+        <h4 className='text-darkgray text-sm font-ibm-plex-mono'>{title}</h4>
+      </div>
+        <Box
+        sx={{
+          display: 'flex',
+          gap: 1,
+          py: 1,
+          overflow: 'auto',
+          width: '100%',
+          scrollSnapType: 'x mandatory',
+          '& > *': {
+            flex: '0 0 auto',
+            width: 'calc(100% - 16px)',
+            scrollSnapAlign: 'center',
+          },
+          '::-webkit-scrollbar': { display: 'none' },
+        }}
+        >
+        {children}
+      </Box>
+    </>
   )
 }
