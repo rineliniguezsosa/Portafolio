c5faf5f3793066dbaf636b7f022d84a24f870e6e
Rinel <rineliniguezsosa@gmail.com>
2025-02-02 19:10:40 -0600

    [FEAT] Guiardando temporalmente el contenido de ProjectCard
    - imoportando el componente Crad de joy
    - importando la interface projectProps
    - desestructurando las props id,name,description, img y url en donde fuerón tipadas por la interface projectProps
    - pegando las mismas props de mi TestimonialCard
    - estableciendo las props img, name,description, url
    - eliminando el fragment

--- src/components/Card/ProjectCard.tsx
+++ src/components/Card/ProjectCard.tsx
@@ -1,39 +1,42 @@
-import Card from '@mui/material/Card';
+import { Card } from "@mui/joy"
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardActions from '@mui/material/CardActions';
+import { projectProps } from "../../types/interfaces";
 
-export const ProjectCard = () => {
+export const ProjectCard = ({id,name,description,img,url}: projectProps) => {
   return (
-    <>
-      <h4>Titulo</h4>
-      <Card sx={{border:'none'}}>
+      <Card
+        key={id}
+        sx={{border:'none'}}
+        orientation="horizontal" 
+        size="sm" 
+        variant="outlined"
+       >
         <CardActionArea>
           <CardMedia
             component="img"
             height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
+            image={img}
             alt="green iguana"
             />
           <CardContent>
             <Typography gutterBottom variant="h3" component="div">
-              Lizard
+              {name}
             </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica
+              {description}
             </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions>
-          <Button size="small" color="primary">
+          <Button href={url} size="small" color="primary">
             Share
           </Button>
         </CardActions>
       </Card>
-    </>
   )
 }
