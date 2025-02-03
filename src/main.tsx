ed2facffddd72b3189500bb3ec2e022e6bf7892f
Rinel <rineliniguezsosa@gmail.com>
2025-02-03 00:41:51 -0600

    [FEAT] Eliminando el high order component BrowserRouter  de nuestra aplicación ya que no queremos que react router este en nuestro navegador
    - importando react
    - volvi a usar react strict mode como envoltoio de mi proyecto principal

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import React from 'react';
 
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
 )
