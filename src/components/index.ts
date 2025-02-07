58e03d8b479ba99649b286bca640a482c16e2465
Rinel <rineliniguezsosa@gmail.com>
2025-02-07 15:07:13 -0600

    [FEAT] Exportando todo del archivo FloatingWhatsapp

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Timeline/TimelineMui'
 export * from './Carrousel/CarrouselMui';
 export * from './Card/TestimonialCard';
 export * from './Card/ProjectCard';
+export * from './buttons/FloatingWhatsapp'
