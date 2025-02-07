f29fdbcb5e85bc39d5f971b968793192163490a3
Rinel <rineliniguezsosa@gmail.com>
2025-02-07 17:01:00 -0600

    [FEAT] Estableciendo nuevo formato al componente FloatingWhatsapp y estableciendo las nuevas props statusMessage, chatMessage

--- src/components/buttons/FloatingWhatsapp.tsx
+++ src/components/buttons/FloatingWhatsapp.tsx
@@ -1,13 +1,22 @@
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 import { Whatsappprops } from '../../types/interfaces'
 
-export const FloatingWhatsapp = ({accountName,phoneNumber,avatar,...props}:Whatsappprops) => {
+export const FloatingWhatsapp = ({
+  accountName,
+  phoneNumber,
+  avatar,
+  statusMessage,
+  chatMessage,
+  ...props}:Whatsappprops
+) => {
   return (
     <FloatingWhatsApp
-    {...props}
-    accountName={accountName} 
-    phoneNumber={phoneNumber} 
-    avatar={avatar}
+      {...props}
+      accountName={accountName} 
+      phoneNumber={phoneNumber} 
+      avatar={avatar}
+      statusMessage={statusMessage}
+      chatMessage={chatMessage}
     />
   )
 }
