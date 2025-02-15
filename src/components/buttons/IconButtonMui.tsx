c01baade8fa1d23d2f329b2f243fd8ca2bb21bf3
Rinel <rineliniguezsosa@gmail.com>
2025-02-14 18:04:46 -0600

    [FEAT] Cambiando el nombre al archivo y al componente ya que lo tenia mal nombrado a IconButtonMui

--- src/components/buttons/IconButtonMui.tsx
+++ src/components/buttons/IconButtonMui.tsx
@@ -2,7 +2,7 @@ import { IconButton } from '@mui/material'
 import { IconButtonProps } from '../../types/interfaces'
 
 
-export const IonButtonMui = ({children,...props}:IconButtonProps) => {
+export const IconButtonMui = ({children,...props}:IconButtonProps) => {
   return (
     <IconButton
         {...props}
