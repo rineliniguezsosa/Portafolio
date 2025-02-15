da07be985033a1d2ee586e7895a17168cfb378e0
Rinel <rineliniguezsosa@gmail.com>
2025-02-14 19:20:26 -0600

    [FEAT] Cambios importantes en el componente CarrouselMui, para lograr este cambio importante necesite de invetigar y pruebas al final considero importante documentarlo
    - importe el hook userRef de react para obtener la referencia de los atributos del dom del elemento html div
    - importe el botón IconButtonMui de buttons, mejore la importación
    - importando los iconos  KeyboardArrowLeftIcon  y KeyboardArrowRightIcon
    - creé la variable scrollcontainer el cual estableceremos la referencia del dom en el elemento div ya que al final de cuentas el box componente de joy es un div
    - creé la función scroll en donde recibe un argumento ya sea left ó right en donde si existe la referencia a este elemento , creo una variable scrollAmount para aumentar el recorrido del scroll, entonces obtengo la prop scrollBy del DOM para desplasar el contenido , hago un ternario para verificar si la dirección es left , y si lo es entonces me desplazo a la izquierda de manera negativa y si no de manera positiva , establezco el desplazamiento en smooth para que sea sueva
    - estableci un contenedor div  nuevo que envuelve los iconos y el carrousel en donde simplemente estableci display flex para darle dirección y orden
    - estableci los iconos en donde estableci los métodos onclick para que este actualice el valor del contenido del carrousel
    - color a los iconos #9C9C9C
    - le estableci al componente Box la referencia del useRef el cual sabremos que moveremos el contenido

--- src/components/Carrousel/CarrouselMui.tsx
+++ src/components/Carrousel/CarrouselMui.tsx
@@ -1,31 +1,56 @@
 import Box from '@mui/joy/Box';
 import { CarrouselMuiProps } from '../../types/interfaces';
-
+import { useRef } from 'react';
+import { IconButtonMui } from '../buttons';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export const CarrouselMui = ({title,children}:CarrouselMuiProps) => {
+  const scrollcontainer = useRef<HTMLDivElement>(null)
+
+  const scroll = (direction:'left' | 'right') =>{
+    if(scrollcontainer.current){
+      const scrollAmount = 300;
+      scrollcontainer.current.scrollBy({
+        left:direction === 'left' ? -scrollAmount : scrollAmount,
+        behavior: 'smooth',
+      })
+      console.log(direction);
+    }
+  }
+ 
   return (
     <>
       <div>
         <h4 className='text-darkgray text-sm font-ibm-plex-mono'>{title}</h4>
       </div>
-        <Box
-        sx={{
-          display: 'flex',
-          gap: 1,
-          py: 1,
-          overflow: 'auto',
-          width: '100%',
-          scrollSnapType: 'x mandatory',
-          '& > *': {
-            flex: '0 0 auto',
-            width: 'calc(100% - 16px)',
-            scrollSnapAlign: 'center',
-          },
-          '::-webkit-scrollbar': { display: 'none' },
-        }}
-        >
-        {children}
-      </Box>
+       <div className='flex flex-row items-center'>
+         <IconButtonMui onClick={() => scroll('left')}>
+            <KeyboardArrowLeftIcon  sx={{color:'#9C9C9C'}}/>
+         </IconButtonMui>
+          <Box
+          ref={scrollcontainer}
+          sx={{
+            display: 'flex',
+            gap: 1,
+            py: 1,
+            overflow: 'auto',
+            width: '100%',
+            scrollSnapType: 'x mandatory',
+            '& > *': {
+              flex: '0 0 auto',
+              width: 'calc(100% - 16px)',
+              scrollSnapAlign: 'center',
+            },
+            '::-webkit-scrollbar': { display: 'none' },
+          }}
+          >
+          {children}
+          </Box>
+          <IconButtonMui onClick={() => scroll('right')}>
+            <KeyboardArrowRightIcon sx={{color:'#9C9C9C'}}/>
+         </IconButtonMui>
+       </div>
     </>
   )
 }
