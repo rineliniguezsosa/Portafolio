873bae845ec38200e76f9d99263f09779111fcb1
Rinel <rineliniguezsosa@gmail.com>
2025-02-14 19:04:53 -0600

    [FEAT] Cambios en el componente IconButtonMui
    - estableciendo la prop size como small para darle tamaño el componente IconButton
    - estableciendo la prop onClick en donde establecemos la referencia del método onClick y pasamos por el momento el valor de left ya que espera un valor
    - desestruramos la prop onClick

--- src/components/buttons/IconButtonMui.tsx
+++ src/components/buttons/IconButtonMui.tsx
@@ -2,9 +2,11 @@ import { IconButton } from '@mui/material'
 import { IconButtonProps } from '../../types/interfaces'
 
 
-export const IconButtonMui = ({children,...props}:IconButtonProps) => {
+export const IconButtonMui = ({children,onClick,...props}:IconButtonProps) => {
   return (
     <IconButton
+        size='small'
+        onClick={() => onClick('left')}
         {...props}
     >
         {children}
